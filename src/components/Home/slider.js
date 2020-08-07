03e689d75116e8cd632fa7103938ddf1e15696d8
RemAlw <nhoxtvt@gmail.com>
2020-08-07 21:54:37 +0700

    add value for slider

--- src/components/Home/slider.js
+++ src/components/Home/slider.js
@@ -12,26 +12,6 @@ class slider extends Component{
                             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                             </ol>
-                            <div className="carousel-inner slider-content--tv_screen">
-                            <div className="carousel-item active slider-content--tv_screen__box">
-                                <img className="d-block w-100 slider-content--tv_screen__box___pic" src="./img/mouse2A.png" alt="First slide"/>
-                                <img className="slider-content--tv_screen__box___pic-shadow" src="/img/shadow.png" alt="shadow-picture"/>
-                                <span className="slider-content--tv_screen__box___introduce"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Remalw</span>
-                                <button id="" className="slider-content--tv_screen__box___btn">Mua Ngay</button>
-                            </div>
-                            <div className="carousel-item  slider-content--tv_screen__box">
-                                <img className="d-block w-100 slider-content--tv_screen__box___pic" src="./img/Razer2A.png" alt="Second slide"/>
-                                <img className="slider-content--tv_screen__box___pic-shadow" src="/img/shadow.png" alt="shadow-picture"/>
-                                <span className="slider-content--tv_screen__box___introduce"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Remalw</span>
-                                <button id="" className="slider-content--tv_screen__box___btn">Mua Ngay</button>
-                            </div>
-                            <div className="carousel-item  slider-content--tv_screen__box">
-                                <img className="d-block w-100 slider-content--tv_screen__box___pic" src="./img/mouse2B.png" alt="Third slide"/>
-                                <img className=" slider-content--tv_screen__box___pic-shadow" src="/img/shadow.png" alt="shadow-picture"/>
-                                <span className="slider-content--tv_screen__box___introduce"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Remalw</span>
-                                <button id="" className="slider-content--tv_screen__box___btn">Mua Ngay</button>
-                            </div>
-                            </div>
                             
                         </div>
                     </div>
