1d37fbbb3c199d474a4698ce9691adad49eedb78
RemAlw <nhoxtvt@gmail.com>
2020-08-07 21:55:23 +0700

    set layout for slider

--- src/components/Home/slider.js
+++ src/components/Home/slider.js
@@ -6,14 +6,7 @@ class slider extends Component{
         return (
             <div className="slider" id="slider">
                     <div className="slider-content">
-                        <div id="carouselExampleIndicators" className="carousel slide slider-content--tv" data-ride="carousel">
-                            <ol className="carousel-indicators slider-content--tv_control">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
-                            
-                        </div>
+                        
                     </div>
                 </div>
         );
